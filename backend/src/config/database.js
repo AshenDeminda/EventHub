b9f45873ce5b25967e795da161d6accdad4817c2
AshenDeminda <114336796+AshenDeminda@users.noreply.github.com>
2025-11-07 23:57:50 +0530

    make to work in DEV mode

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -4,10 +4,7 @@ const connectDB = async () => {
   try {
     const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://admin:password123@mongodb:27017/event-scheduler?authSource=admin';
     
-    await mongoose.connect(MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(MONGODB_URI);
     
     console.log('Connected to MongoDB');
   } catch (error) {
